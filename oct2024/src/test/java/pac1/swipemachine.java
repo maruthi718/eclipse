c21c2036227a2034a578d03ec07829ae6d3ec077
maruthi <maruthikunuku718@gmail.com>
2024-10-15 15:52:04 +0530

    first

---
+++ oct2024/src/test/java/pac1/swipemachine.java
@@ -0,0 +1,12 @@
+package pac1;
+
+public class swipemachine {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+           swipemachine1 obj =new chipcardmachine();
+           obj1.readcard();
+           
+	}
+
+}
